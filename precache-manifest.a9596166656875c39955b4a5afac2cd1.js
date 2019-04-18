fa5e299fb5ec82bed222400fbcfe9b3f31b9446d
nour202 <nourelhoudamejri20@gmail.com>
2019-04-18 14:18:10 +0200

    Updates

---
+++ precache-manifest.a9596166656875c39955b4a5afac2cd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "d584e0cfb4fa99e998df",
+    "url": "/contactmanager/static/js/main.c2735663.chunk.js"
+  },
+  {
+    "revision": "16f7a46bdc154af51256",
+    "url": "/contactmanager/static/js/2.9f109a9d.chunk.js"
+  },
+  {
+    "revision": "d584e0cfb4fa99e998df",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "16f7a46bdc154af51256",
+    "url": "/contactmanager/static/css/2.5ce8ebc6.chunk.css"
+  },
+  {
+    "revision": "1becdbdc43e5a4027c669844f07f15cf",
+    "url": "/contactmanager/index.html"
+  }
+];
